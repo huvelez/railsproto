c0d7e0b2f047ea50e83ab7e1bd1e46ace29ad874
Hector Velez <hvelez@sapient.com>
2016-07-12 14:14:04 -0400

    Initial Commit

---
+++ app/models/person.rb
@@ -0,0 +1,19 @@
+
+class Person
+
+  include Mongoid::Document
+
+
+
+
+  field :name, type: String
+
+  field :street, type: String
+
+  field :city, type: String
+
+  field :state, type: String
+
+
+end
+
